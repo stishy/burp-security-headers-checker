d0f01cba53c5c97d1db77d1533b1311edd928574
stishy <cameronjstish@gmail.com>
2024-01-23 14:20:06 -0500

    Update MissingSecurityHeaderIssue.java

--- src/main/java/burp/MissingSecurityHeaderIssue.java
+++ src/main/java/burp/MissingSecurityHeaderIssue.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 class MissingSecurityHeaderIssue implements IScanIssue {
-    private String pathData;
     private static final int ISSUE_TYPE = 0;
     private static final String SEVERITY = "Low";
     private static final String CONFIDENCE = "Certain";
@@ -18,26 +17,18 @@ class MissingSecurityHeaderIssue implements IScanIssue {
             "<li><a href=https://cheatsheetseries.owasp.org/cheatsheets/Content_Security_Policy_Cheat_Sheet.html>OWASP CSP Cheat Sheet</a></li>" +
             "<li><a href=https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Strict_Transport_Security_Cheat_Sheet.html>OWASP HSTS Cheat Sheet</a></li>" +
             "<li><a href=https://cheatsheetseries.owasp.org/cheatsheets/Clickjacking_Defense_Cheat_Sheet.html>OWASP Clickjacking Defense Cheat Sheet</a></li></ul>";
-
     private final java.net.URL url;
     private final String issueName;
-    private String issueDetail; // Not final anymore
+    private final String issueDetail;
     private final IHttpRequestResponse[] httpMessages;
     private final IHttpService httpService;
 
-    // Modify the constructor to initialize issueDetail
     public MissingSecurityHeaderIssue(java.net.URL url, String issueName, String issueDetail, IHttpRequestResponse[] httpMessages, IHttpService httpService) {
-    this.url = url;
-    this.issueName = issueName;
-    this.issueDetail = issueDetail;
-    this.httpMessages = httpMessages;
-    this.httpService = httpService;
-}
-
-    // Modify the appendPathData method to format pathData differently
-    public void appendPathData(String pathData) {
-        // Format pathData as a bullet point and add it to the issue detail
-        this.issueDetail += "<ul><li>Hi " + pathData + "</li></ul>";
+        this.url = url;
+        this.issueName = issueName;
+        this.issueDetail = issueDetail;
+        this.httpMessages = httpMessages;
+        this.httpService = httpService;
     }
 
     @Override
@@ -72,13 +63,6 @@ class MissingSecurityHeaderIssue implements IScanIssue {
 
     @Override
     public String getIssueDetail() {
-        // Split the issueDetail by line breaks and wrap each line in <li> tags
-        String[] lines = issueDetail.split("\n");
-        StringBuilder sb = new StringBuilder("<ul>");
-        for (String line : lines) {
-            sb.append("<li>").append(line).append("</li>");
-        }
-        sb.append("</ul>");
-        return sb.toString();
+        return issueDetail;
     }
 }
